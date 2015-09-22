fc3f099bd6b18bf17afc38c4cc6ce8eff98d5d33
Isabella <isabella.silveira.souza@gmail.com>
2015-09-22 00:23:46 -0300

    Arquivos do Jota Poeta
    Upando os arquivos da extensão

---
+++ main.js
@@ -0,0 +1,14 @@
+$(function() {
+        var cor = '#';
+        var codigoCorHex = ['064d41','d44d4d','35a59a','6e0892','c83759','126518','941616','d49703', '0a0a2e', 'c63b04'];
+        cor += codigoCorHex[Math.floor(Math.random() * codigoCorHex.length)];
+        document.body.style.background = cor;
+
+    $.getJSON("frases.json", function(data) {
+        console.log(data.frases.length);
+        var listaFrases = data.frases;
+        var fraseRandom = listaFrases[Math.floor(Math.random()*listaFrases.length)];
+        document.getElementById("posts").innerHTML = fraseRandom.poesia;
+
+    });
+});
